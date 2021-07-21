8d6ce994fde2b712d44963c80eba224280f6d747
John Graves <johntaylorgraves@gmail.com>
2021-07-21 16:39:21 +0100

    added totals

--- parse.py
+++ parse.py
@@ -13,23 +13,30 @@ with open('input.log', 'r') as log:
         if (regExp1.search(line)):
             regExp3 = re.compile("(?<=\>)(\d*[0-9][.]\d*[0-9])")
             if (regExp3.search(line)):
-                #print("you have hit for:")
-                #re.search(regExp3,line)
-                #print(re.search(regExp3,line).group())
                 hit.append(re.search(regExp3,line).group())
         
         regExp2 = re.compile("(has hit)")
         if (regExp2.search(line)):
             regExp3 = re.compile("(?<=\>)(\d*[0-9][.]\d*[0-9])")
             if (regExp3.search(line)):
-                #print("has hit you for:")
-                #print(re.search(regExp3,line).group())
                 tookHit.append(re.search(regExp3,line).group())
     
     with open("output.csv", "w", encoding="UTF8", newline="") as outputFile:
         writer = csv.writer(outputFile)
+        
+        totalHit = 0
+        totalTookHit = 0
+        
+        for value in hit:
+            totalHit += float(value)
+        for value in tookHit:
+            totalTookHit += float(value)
+        
         writer.writerow([hit])
+        writer.writerow([str(totalHit)])
+        
         writer.writerow([tookHit])
+        writer.writerow([str(totalTookHit)])
 
 # to create exe:
 # http://www.pyinstaller.org/
